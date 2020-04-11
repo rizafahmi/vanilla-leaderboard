3e4505ff53ce61878d4e10a1275336c8114a5405
Riza Fahmi <rizafahmi@gmail.com>
2020-04-11 21:34:52 +0700

    Rendering template with data

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
-console.log('App.js');
+const data = [
+  { id: 1, name: 'Dan Abramov', score: 50 },
+  { id: 2, name: 'Evan You', score: 30 },
+  { id: 3, name: 'Rich Harris', score: 15 }
+];
+console.log(data);
+
+const li = document.getElementById('list-item').innerHTML;
+let newLi = '';
+
+for (let i = 0; i < data.length; i++) {
+  newLi += li
+    .replace(/{{index}}/g, i)
+    .replace(/{{name}}/g, data[i]['name'])
+    .replace(/{{score}}/g, data[i]['score']);
+}
+
+document.getElementById('list').innerHTML = newLi;
