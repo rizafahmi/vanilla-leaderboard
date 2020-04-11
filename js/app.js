23d9702e34360659e8029a72af29613b00bba283
Riza Fahmi <rizafahmi@gmail.com>
2020-04-11 21:56:16 +0700

    Render, re-render, sort and finish form input

--- js/app.js
+++ js/app.js
@@ -3,16 +3,35 @@ const data = [
   { id: 2, name: 'Evan You', score: 30 },
   { id: 3, name: 'Rich Harris', score: 15 }
 ];
-console.log(data);
 
-const li = document.getElementById('list-item').innerHTML;
-let newLi = '';
+function render() {
+  const li = document.getElementById('list-item').innerHTML;
+  let newLi = '';
 
-for (let i = 0; i < data.length; i++) {
-  newLi += li
-    .replace(/{{index}}/g, i)
-    .replace(/{{name}}/g, data[i]['name'])
-    .replace(/{{score}}/g, data[i]['score']);
+  let newList = data
+    .sort((a, b) => parseInt(b.score) - parseInt(a.score))
+    .map((d, i) => {
+      return li
+        .replace(/{{index}}/g, i)
+        .replace(/{{name}}/g, d.name)
+        .replace(/{{score}}/g, d.score);
+    });
+
+  document.getElementById('list').innerHTML = newList.join('');
 }
 
-document.getElementById('list').innerHTML = newLi;
+render();
+
+const form = document.querySelector('form');
+form.onsubmit = function(event) {
+  event.preventDefault();
+
+  const nameInput = document.querySelector('#name');
+
+  const name = nameInput.value;
+  const score = Math.floor(Math.random() * 100);
+
+  data.push({ name, score });
+  nameInput.value = '';
+  render();
+};
