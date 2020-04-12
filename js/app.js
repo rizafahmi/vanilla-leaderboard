27e4fcbfc0f33c0b88c582f5fdcace6b045f4e9a
Riza Fahmi <rizafahmi@gmail.com>
2020-04-12 08:24:05 +0700

    Finish basic functionality: * List sorted by score * Increase score * Decrease score * Add new player

--- js/app.js
+++ js/app.js
@@ -1,8 +1,12 @@
 const data = [
-  { id: 1, name: 'Dan Abramov', score: 50 },
-  { id: 2, name: 'Evan You', score: 30 },
-  { id: 3, name: 'Rich Harris', score: 15 }
+  { id: lil.uuid(), name: 'Dan Abramov', score: 50 },
+  { id: lil.uuid(), name: 'Evan You', score: 30 },
+  { id: lil.uuid(), name: 'Rich Harris', score: 15 }
 ];
+
+function randomize() {
+  return Math.floor(Math.random() * 100);
+}
 
 function render() {
   const li = document.getElementById('list-item').innerHTML;
@@ -12,12 +16,34 @@ function render() {
     .sort((a, b) => parseInt(b.score) - parseInt(a.score))
     .map((d, i) => {
       return li
-        .replace(/{{index}}/g, i)
+        .replace(/{{id}}/g, d.id)
         .replace(/{{name}}/g, d.name)
         .replace(/{{score}}/g, d.score);
     });
 
   document.getElementById('list').innerHTML = newList.join('');
+
+  // Attach onclick
+  const incrButtons = document.querySelectorAll('.btn-success');
+  const decrButtons = document.querySelectorAll('.btn-danger');
+
+  for (let i = 0; i < incrButtons.length; i++) {
+    incrButtons[i].onclick = function() {
+      const id = incrButtons[i].dataset.id;
+      const changeData = data.find((d) => d.id === id);
+      const index = data.indexOf(changeData);
+      data[index].score = data[index].score + randomize();
+      render();
+    };
+
+    decrButtons[i].onclick = function() {
+      const id = decrButtons[i].dataset.id;
+      const changeData = data.find((d) => d.id === id);
+      const index = data.indexOf(changeData);
+      data[index].score = data[index].score - randomize();
+      render();
+    };
+  }
 }
 
 render();
@@ -29,9 +55,10 @@ form.onsubmit = function(event) {
   const nameInput = document.querySelector('#name');
 
   const name = nameInput.value;
-  const score = Math.floor(Math.random() * 100);
+  const score = randomize();
+  const id = lil.uuid();
 
-  data.push({ name, score });
+  data.push({ id, name, score });
   nameInput.value = '';
   render();
 };
